b879f537f574fa3bea099ba940b8e8c09cb5c3f6
davidmasdev <davidmas.sm@gmail.com>
2025-08-22 20:10:07 +0200

    Solved problem 4A

---
+++ codeforces/4A_watermelon.cpp
@@ -0,0 +1,15 @@
+// David Mas
+// https://codeforces.com/problemset/problem/4/A 
+// brute force, math, 800
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    if(w%2==0 && w>2)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
